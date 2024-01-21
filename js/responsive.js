0829f019b0c4639f12473a99bf9ec5ccd6523fde
Giviko <chelidze.givia@gmail.com>
2024-01-21 17:29:03 +0400

    Added responsive menu

---
+++ js/responsive.js
@@ -0,0 +1,58 @@
+const menu = document.querySelector(".menu");
+const menuContainer = document.querySelector(".menu-container");
+const menuContainerContent = document.querySelector(".menu-container-content");
+const spanParent = document.querySelector(".menu__span-parent");
+const menuTop = document.querySelector(".menu__top");
+const menuBottom = document.querySelector(".menu__bottom");
+let lastKnownScrollPosition = 0;
+
+menu.addEventListener("click", () => {
+  spanParent.classList.toggle("menu__span-parent--active");
+  menuTop.classList.toggle("menu__top--animated");
+  menuBottom.classList.toggle("menu__bottom--animated");
+  menuContainer.classList.toggle("menu-container-open");
+  menuContainerContent.classList.toggle("menu-container-content-open");
+});
+
+window.addEventListener("scroll", updateHeaderClass);
+window.addEventListener("scroll", updateHeaderVisibility);
+
+function updateHeaderClass() {
+  const header = document.querySelector("header");
+  const scrollDistance = window.scrollY;
+
+  const threshold = 1;
+
+  if (scrollDistance > threshold) {
+    header.classList.add("header-transparent");
+  } else {
+    header.classList.remove("header-transparent");
+  }
+}
+
+function updateHeaderVisibility() {
+  if (window.innerWidth > 320) return;
+  const header = document.querySelector("header");
+  const menu = document.querySelector(".menu");
+  const currentScrollPosition = window.scrollY;
+
+  if (currentScrollPosition > lastKnownScrollPosition) {
+    header.classList.add("header-hidden");
+    menu.classList.add("header-hidden");
+  } else {
+    header.classList.remove("header-hidden");
+    menu.classList.remove("header-hidden");
+  }
+
+  lastKnownScrollPosition = currentScrollPosition;
+}
+
+function toggleMenu() {}
+
+function disableScroll() {
+  let scrollPosition = window.scrollY;
+
+  // Set the body to fixed position to prevent scrolling
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${scrollPosition}px`;
+}
